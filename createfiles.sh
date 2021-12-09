799d4b87af964d704efa36de6f6fe2299ceca12b
Radomír Bosák <radomir.bosak@gmail.com>
2021-12-09 11:24:21 +0900

    Delete 90% of java files

--- createfiles.sh
+++ createfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for i in {1..400}; do
+for i in {1..40}; do
   for j in {1..250}; do
     mkdir -p "data/$i"
     cp sample.java "data/$i/file$j.java"
   done
-  echo "$i/400"
+  echo "$i/40"
 done
--- data/218/file174.java
+++
@@ -1,8 +0,0 @@
-package sample.checkstyle;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Checkstyle!!");
-    }
-    public void bark() {}
-}
